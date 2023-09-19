1fef534ec85fbf1d05315f86e011128f56555957
Syed Maaz <63224539+syedmaaz388@users.noreply.github.com>
2023-09-19 09:52:33 +0530

    Delete my-app directory

--- my-app/src/UseToggle.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-export const UseToggle = (initialVal = false) => {
-    const [state,setState] = useState(initialVal);
-    const toggle = () => {
-        setState((prev) => !prev);
-    };
-    return [state,toggle];
-}
